65b596d63cbd6738a040e46209996ea2e1fc7fe9
402-27-Kvisara <54401@yupparaj.ac.th>
2026-08-19 15:23:43 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -8,8 +8,10 @@ if "ans1_val" not in st.session_state:
     st.session_state.ans1_val = ""
 if "ans2_val" not in st.session_state:
     st.session_state.ans2_val = ""
-
-
+if "ans3_val" not in st.session_state:
+    st.session_state.ans3_val = ""
+if "ans4_val" not in st.session_state:
+    st.session_state.ans4_val = ""
 # 📌 ฟังก์ชันเคลียร์ค่าเมื่อกดปุ่มเริ่มใหม่
 def reset_game():
     st.session_state.ans1_val = ""  # เคลียร์ค่าช่องข้อ 1
@@ -42,15 +44,19 @@ def show_result_dialog(ans1, ans2):
         score += 1
     else:
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
-
-    # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4 ตรงนี้
-
-    st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
-
-    if score == 2:
-        st.success("🎉 You win!")
+ # ตรวจข้อ 3
+    if u_ans3 == "Blueberry":
+        st.success("✅ ข้อ 3: ถูกต้อง")
+        score += 4
     else:
-        st.error("💀 You lose!")
+        st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')") 
+         # ตรวจข้อ 4
+    if u_ans4 == "Peach":
+        st.success("✅ ข้อ 3: ถูกต้อง")
+        score += 4
+    else:
+        st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
+   
 
 
 # ----------------------------------------------------
@@ -79,12 +85,19 @@ ans2 = st.text_input(
     "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
     value=st.session_state.ans2_val,
 )
-
+ans3 = st.text_input(
+    "ข้อ 3: Cats love to eat `B _ u e _ e r r _`. 🫐",
+    value=st.session_state.ans3_val,
+)
+ans4 = st.text_input(
+    "ข้อ 4: Cats love to eat `P e _ c _`.🍑" ,
+    value=st.session_state.ans4_val,
+)
 # อัปเดตค่าล่าสุดเข้าตัวแปร
 st.session_state.ans1_val = ans1
 st.session_state.ans2_val = ans2
-
-# ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มข้อ 3, 4 ตรงนี้
+st.session_state.ans3_val = ans3
+st.session_state.ans4 _val = ans4
 
 
 # 4. ปุ่มส่งคำตอบ
@@ -98,7 +111,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2)
+    show_result_dialog(ans1, ans2,ans3,ans4)
 
 st.divider()
 st.write("นางสาวกวิสรา สุระภาพ เลขที่ 27 ม.4/2")
